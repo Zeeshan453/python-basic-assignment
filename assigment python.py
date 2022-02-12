446752f0d1f8a1fdabba23a1bbf992984d72a71a
Zeeshan453 <96932649+Zeeshan453@users.noreply.github.com>
2022-02-12 22:05:00 +0530

    Add files via upload

---
+++ assigment python.py
@@ -0,0 +1,42 @@
+a= "let's learn some Python today!"
+print(a) ##n0. 01
+print(a.upper())##n0. 02
+print(a.lower())##n0. 02
+print(a.swapcase())##n0. 02
+name = input('enter your name')
+
+print(f"hello {name} {a}" ) ##n0.03
+
+famous_person = "A. P. J. Abdul Kalam "  ## n0.4
+message = "you  have to dream before your dream can came true."
+
+print(f" sir {famous_person} said:{message}") ## n0.5
+
+print("5+3=8") ## n0.6
+print("9-1=8")  ## n0.6
+print("16%2=8")   ## n0.6
+print("4*2=8") ## n0.6
+
+B2=8 
+
+print(f" my favourite number is {B2} ")## n0. 08
+
+C2 = ("Rahul , Manish , jay , jigar")
+C3= C2.split(',')
+
+
+for C1 in C3 :   ###n0.09
+    print("my friendship with",C1," is start fr0m College") ###n0.10
+
+
+    
+
+
+
+
+
+
+
+
+
+
